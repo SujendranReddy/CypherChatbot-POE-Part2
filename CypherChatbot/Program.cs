22ce5dc5495e3c8035123dd86b2d3bc71d6d3e1d
Sujendran Reddy <ST10457761@vcconnect.edu.za>
2025-04-22 16:55:03 +0200

    Added custom console title, PrintBanner() method for ASCII art, DrawDivider() to organize the conversation, and PrintWithEffect() to introduce a human like typing effect.

--- CypherChatbot/Program.cs
+++ CypherChatbot/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+using System.Threading;
 
-namespace CypherChatbot
+namespace CypherChatBot
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Cypher - Your Cybersecurity Companion";
+            PrintBanner();
+        }
+
+        static void PrintBanner()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            PrintWithEffect(@"
+_________                  .__                     
+\_   ___ \  ___.__.______  |  |__    ____  _______ 
+/    \  \/ <   |  |\____ \ |  |  \ _/ __ \ \_  __ \
+\     \____ \___  ||  |_> >|   Y  \  ___/  |  | \/
+ \______  / / ____||   __/ |___|  /\___  > |__|   
+        \/  \/     |__|         \/     \/        
+");
+            Console.ResetColor();
+        }
+
+        static void DrawDivider(string title = "")
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            if (!string.IsNullOrWhiteSpace(title))
+                Console.WriteLine($"\n==== {title.ToUpper()} =============================");
+            else
+                Console.WriteLine("\n===================================================");
+            Console.ResetColor();
+        }
+
+        static void PrintWithEffect(string text, int delay = 10)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(delay);
+            }
         }
     }
 }
